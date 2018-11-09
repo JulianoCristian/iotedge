384d25eef56df61c04e3f1862c31e24dabdbb998
Philip Lin <philipktlin@live.com>
2018-11-09 10:51:11 -0800

    add routes controller to list out defined route for investigation

---
+++ edge-hub/src/Microsoft.Azure.Devices.Edge.Hub.Http/controllers/RoutesController.cs
@@ -0,0 +1,62 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+namespace Microsoft.Azure.Devices.Edge.Hub.Http.controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.AspNetCore.Authorization.Infrastructure;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Abstractions;
+    using Microsoft.AspNetCore.Mvc.Authorization;
+    using Microsoft.AspNetCore.Mvc.Infrastructure;
+    using Microsoft.AspNetCore.Mvc.Internal;
+
+    [Route("monitor")]
+    public class MonitorController : Controller
+    {
+        readonly IActionDescriptorCollectionProvider provider;
+
+        public MonitorController(IActionDescriptorCollectionProvider provider)
+        {
+            this.provider = provider;
+        }
+
+        [HttpGet("routes")]
+        public IActionResult GetRoutes()
+        {
+            IEnumerable<ActionDescriptor> openRoutes = this.provider.ActionDescriptors.Items
+                .Where(
+                    x => x.FilterDescriptors.All(f => f.Filter.GetType() != typeof(AuthorizeFilter)) ||
+                        x.FilterDescriptors.Any(f => f.Filter.GetType() == typeof(AllowAnonymousFilter)));
+
+            var openRoutesDisplay = openRoutes
+                .Select(x => $"{x?.ActionConstraints?.OfType<HttpMethodActionConstraint>().FirstOrDefault()?.HttpMethods.First()} -> {x.AttributeRouteInfo.Template}");
+
+            var roleGroupedRoutesDisplay = this.provider.ActionDescriptors.Items
+                .Except(openRoutes)
+                .GroupBy(r => this.GetAuthorizationRole(r))
+                .SelectMany(
+                    g =>
+                        g.Select(x => $"[{g.Key}] {x?.ActionConstraints?.OfType<HttpMethodActionConstraint>().FirstOrDefault()?.HttpMethods.First()} -> {x.AttributeRouteInfo.Template}")
+                ).ToArray();
+            return this.Ok(
+                openRoutesDisplay
+                    .Concat(new[] { "-------- SECURED ROUTES --------" })
+                    .Concat(roleGroupedRoutesDisplay));
+        }
+
+        public string GetAuthorizationRole(ActionDescriptor action)
+        {
+            var allowedRoles = ((RolesAuthorizationRequirement)action.FilterDescriptors.Where(x => x.Filter.GetType() == typeof(AuthorizeFilter))
+                .SelectMany(x => ((AuthorizeFilter)x.Filter).Policy.Requirements)
+                .FirstOrDefault(x => x.GetType() == typeof(RolesAuthorizationRequirement)))?.AllowedRoles;
+
+            if (allowedRoles == null)
+            {
+                return "Authenticated";
+            }
+
+            return string.Join(", ", allowedRoles);
+        }
+    }
+}
